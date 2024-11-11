50dcdeb779b4d5262d60adaa72be0c0b84a6a64d
oliwii <olivia.tadini@gmail.com>
2024-11-11 21:14:55 +0000

    g

--- pages/input.py
+++ pages/input.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import altair as alt
+import pandas as pd
 
 st.title("Insert content")
 
@@ -35,12 +37,20 @@ st.write(f"You wrote {len(txt)} characters.",
 if st.button(":material/send: Submit"):
     st.switch_page("pages/output.py")
 
+bias_level = [
+    "Bias free",
+    "Weakly biased",
+    "Moderately biased",
+    "Highly biased",
+    "Extremely biased"
+]
+
 input_dict = {
     "BiasList": [
             {
                 "BiasId": 1,
                 "BiasType": "Gender",
-                "BiasDegree": "Red",
+                "BiasDegree": "Highly biased",
                 "ConfidencePercentage": 80,
                 "FragmentsPresent": {
                     "items": [
@@ -79,4 +89,57 @@ input_dict = {
         "Subtopic": "Election Security"
 }
 
+colour_reference = {
+     "Bias free": 0,
+     "Weakly biased": 1,
+     "Moderately biased": 2,
+     "Highly biased": 3,
+     "Extremely biased": 4
+}
+
+colour_rgb = [
+    "#6ABD45",
+    "#FFC107",
+    "#FF9800",
+    "#F44336",
+    "#D32F2F"
+]
+
+# Obtain the biases types from the output
+# Obtain the degrees or categorization for each bias from the output
+categories = []
+degrees = []
+colors = []
+for item in input_dict["BiasList"]:
+    categories.append(item["BiasType"])
+    degrees.append(item["BiasDegree"])
+    colors.append(colour_reference[item["BiasDegree"]])
+
+# Define colors for each bias level
+color_scale = alt.Scale(
+    domain=bias_level,
+    range=colour_rgb
+)
+
+# Define the data in tabular format
+data = pd.DataFrame({
+    'Category': categories,
+    'Degree': degrees,
+    'Bias_Index': colors  # These indices map to the colors
+})
+
+# Create the Altair chart
+chart = alt.Chart(data).mark_bar().encode(
+    y=alt.Y('Category', sort='-x', title=""),
+    x=alt.X('Bias_Index:O', axis=alt.Axis(title="Nivel de Sesgo", labels=False)),
+    color=alt.Color('Degree', scale=color_scale, legend=alt.Legend(title="Nivel de Sesgo"))
+).properties(
+    title="Análisis de Contenido",
+    width=400,
+    height=200
+)
+
+# Display the chart
+chart
+   
 
