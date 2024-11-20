f5f6d9f349ab562330ec5a57f80499be0c91f5e2
Olivia Tadini <olivia.tadini@gmail.com>
2024-11-20 11:27:39 -0300

    ak

--- pages/input.py
+++ pages/input.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from references.max import max
 from references.prompttemplate import template
 #from langchain.llms import OpenAI
 from langchain_core.prompts import PromptTemplate
@@ -57,7 +58,7 @@ original_text_area = st.text_area(
 )
 
 st.write(
-    f"You wrote {len(original_text_area)} characters.",
+    f"You wrote {max(0,{len(original_text_area)})} characters.",
     "\n"
 )
 
@@ -90,7 +91,7 @@ if st.button(":material/send: Submit"):
             api_key=st.session_state.openai_api_key
             )
         st.session_state.result = llm.invoke(prompt_value)
-        #st.write(result)
+        st.write(st.session_state.result)
         st.write("See results in Output page:")
         st.page_link(
             page="pages/output.py",
@@ -103,8 +104,3 @@ if st.button(":material/send: Submit"):
             page="pages/settings.py",
             label="Go to Settings :material/arrow_forward:"
         )
-
-
-"""
-{"content":"{\n    \"BiasList\": [\n        {\n            \"BiasId\": 1,\n            \"BiasType\": \"Gender\",\n            \"BiasDegree\": \"Highly biased\",\n            \"ConfidencePercentage\": 80,\n            \"FragmentsPresent\": [\n                {\n                    \"FragmentId\": 1,\n                    \"FragmentContent\": \"Los hombres manejan mejor que las mujeres.\",\n                    \"FragmentBiasDegree\": \"Highly biased\",\n                    \"Reformulations\": [\n                        {\n                            \"ReformulationId\": 1,\n                            \"ReformulationLevel\": \"Simple\",\n                            \"AlternativeText\": \"Los hombres son mejores conductores que las mujeres.\"\n                        },\n                        {\n                            \"ReformulationId\": 2,\n                            \"ReformulationLevel\": \"Medium\",\n                            \"AlternativeText\": \"Se dice que los hombres tienen más habilidades al volante que las mujeres.\"\n                        },\n                        {\n                            \"ReformulationId\": 3,\n                            \"ReformulationLevel\": \"Complex\",\n                            \"AlternativeText\": \"Existen afirmaciones que sugieren que los hombres poseen una mayor destreza en la conducción en comparación con las mujeres.\"\n                        }\n                    ]\n                }\n            ],\n            \"Explanation\": \"El sesgo de género es un sesgo definido como la creencia de que un género es superior o inferior a otro en ciertas habilidades o características.\"\n        }\n    ],\n    \"TuringTest\": true,\n    \"Category\": \"\",\n    \"Topic\": \"\",\n    \"Subtopic\": \"\"\n}","additional_kwargs":{"refusal":null},"response_metadata":{"token_usage":{"completion_tokens":298,"prompt_tokens":522,"total_tokens":820,"completion_tokens_details":{"accepted_prediction_tokens":0,"audio_tokens":0,"reasoning_tokens":0,"rejected_prediction_tokens":0},"prompt_tokens_details":{"audio_tokens":0,"cached_tokens":0}},"model_name":"gpt-4o-mini-2024-07-18","system_fingerprint":"fp_3de1288069","finish_reason":"stop","logprobs":null},"type":"ai","name":null,"id":"run-600cd61f-fba2-41d6-bee8-25638ac3e2e7-0","example":false,"tool_calls":[],"invalid_tool_calls":[],"usage_metadata":{"input_tokens":522,"output_tokens":298,"total_tokens":820,"input_token_details":{"audio":0,"cache_read":0},"output_token_details":{"audio":0,"reasoning":0}}}
-"""
---
+++ references/max.py
@@ -0,0 +1,5 @@
+def max(n1,n2):
+    if n1 <= n2:
+        return n2
+    else:
+        return n1
